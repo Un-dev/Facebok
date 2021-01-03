cc552773ef61a3514bc16c461be5f2d195f47a2f
Francois Lannoy <flannoy154@gmail.com>
2021-01-03 01:37:34 +0100

    :construction: posting a user works (more or less)

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,33 @@
-import express from 'express';
+import express from 'express'
+import User from '../models/user.model.js'
+import mongoose from 'mongoose'
 
 const users = express.Router();
 
 users.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('hello world')
 });
 
 users.get('/:id', (req, res) => {
 
 });
 
-users.post('/', (req, res) => {
+users.post('/', async(req, res, err) => {
+  
+  try{
+    const{username} = req.body;
+    // console.log(req.req.body)
 
-});
+    let user = {}
+    user._id = new mongoose.mongo.ObjectId()
+    user.username = username
+    let userModel = new User(user)
+    await userModel.save()
+    res.send().json(userModel)
+  }catch(err){
+    console.log(err)
+  }
+})
+
 
 export default users;
