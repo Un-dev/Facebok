5ea72d1ef91760f90a601fd982a27751e7132f84
Francois Lannoy <flannoy154@gmail.com>
2021-01-03 14:47:29 +0100

    :sparkles: added get / and get /:id
    improved post /

--- models/user.model.js
+++ models/user.model.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose'
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: mongoose.Schema.Types.String,
-    // password: mongoose.Schema.Types.String,
+    password: mongoose.Schema.Types.String,
     // age: mongoose.Schema.Types.Decimal128,
 })
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema, 'users')
 
 export default User
--- routes/users.js
+++ routes/users.js
@@ -2,31 +2,55 @@ import express from 'express'
 import User from '../models/user.model.js'
 import mongoose from 'mongoose'
 
+
+
 const users = express.Router();
 
-users.get('/', (req, res) => {
-  res.send('hello world')
+//shows raw password
+users.get('/', (req, res, err) => {
+  User.find()
+      .then(docs => {
+        console.log(`users found : \n${docs}`)
+        res.status(200).json({
+          count: docs.length,
+          users: docs
+        })
+      })
 });
 
-users.get('/:id', (req, res) => {
-
+users.get('/:id', (req, res, erre) => {
+  User.findById(new mongoose.mongo.ObjectId(req.params.id))
+  .then(user => {
+    res.status(200).json(user)
+  })
 });
 
+//can have same password/username etc as others
 users.post('/', async(req, res, err) => {
-  
   try{
-    const{username} = req.body;
-    // console.log(req.req.body)
+    const{username, password} = req.body;
+    console.log(req.body)
 
-    let user = {}
-    user._id = new mongoose.mongo.ObjectId()
-    user.username = username
+    const id = new mongoose.mongo.ObjectId()
+
+    let user = {
+      _id: id,
+      username: username,
+      password: password
+    }
     let userModel = new User(user)
     await userModel.save()
-    res.send().json(userModel)
+    User.findById(id)
+        .then(createdUser => {
+          res.status(201).json(createdUser)
+        })
   }catch(err){
     console.log(err)
   }
+})
+
+users.delete('/:id', (req, res, err)=>{
+
 })
 
 
